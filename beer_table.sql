91b0cc4e1138029e591c56463178468f7fa2b30c
Andrei Mikhailouski <andrei.mikhailouski@devteam.inc>
2024-08-08 16:02:23 +0300

    change style

--- beer_table.sql
+++ beer_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE beer_table (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     brewery VARCHAR(100) NOT NULL,
-    style VARCHAR(50),
+    style VARCHAR(55),
     alcohol_content DECIMAL(4, 2),
     volume_ml INT
 );
