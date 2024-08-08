cc1704819f1f8e3750abb3a5059b2e1ed51d9679
Andrei Mikhailouski <andrei.mikhailouski@devteam.inc>
2024-08-08 16:00:58 +0300

    true_dvach

---
+++ beer_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE beer_table (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    brewery VARCHAR(100) NOT NULL,
+    style VARCHAR(50),
+    alcohol_content DECIMAL(4, 2),
+    volume_ml INT
+);
